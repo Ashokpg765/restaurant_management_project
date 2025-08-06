1ba39797f02a1fa0173de9d0056921b9a50c6c69
Ashokpg765 <pg7559392@gmail.com>
2025-08-06 11:31:21 +0530

    22076

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
+from django.conf import settings
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    context={
+        "resturant_name":settings.RESTURANT_NAME
+    }
+    return render(request,"home/index.html",context)
